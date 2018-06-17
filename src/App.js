d821477ae91e4b56ec20f604cf8569ce1871883d
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-06-17 20:25:52 -0300

    added component to generate tokens

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import InfoProduction from './Components/InfoProduction/InfoProduction';
 import Generator from './Components/Generator/Generator';
 import TableProduction from './Components/TableGLC/TableProduction';
 import TableGenerator from './Components/TableGLC/TableGenerator';
+import GenerateToken from './Components/GenerateToken/GenerateToken';
 
 class App extends Component {
 	state = {
@@ -126,41 +127,8 @@ class App extends Component {
 	};
 
 	handleGenerateToken = () => {
-		const production = {
-			S: [ 'bBa', 'cC' ],
-			A: [ 'cCa', 'bS', 'ε' ],
-			B: [ 'cCA', 'b' ],
-			C: [ 'Bd', 'a' ]
-		};
-		let generateToken = '';
-		let charReplace = '';
-		let keyObj = 'S';
-		let sizeRandom;
-
-		sizeRandom = Math.floor(Math.random() * production[keyObj].length);
-		generateToken = production[keyObj][sizeRandom];
-		let count = 0;
-
-		let i;
-		while (count < 800 && generateToken !== generateToken.toLowerCase) {
-			count += 1;
-
-			for (i = 0; i < generateToken.length && !this.isUpperCase(generateToken.charAt(i)); i++) {}
-			if (generateToken.length === i) {
-				break;
-			}
-			keyObj = generateToken.charAt(i);
-			sizeRandom = Math.floor(Math.random() * production[keyObj].length);
-			charReplace = production[keyObj][sizeRandom];
-			if (charReplace === 'ε') {
-				generateToken = generateToken.replace(keyObj, '');
-			} else {
-				generateToken = generateToken.replace(keyObj, charReplace);
-			}
-		}
-
 		this.setState({
-			inputValue: generateToken
+			inputValue: GenerateToken()
 		});
 	};
 
